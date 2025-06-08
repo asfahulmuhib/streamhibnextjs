36d9b21ba446854de09fabbba12b55cefc94d514
asfahulmuhib <asfahulmuhib@gmail.com>
2025-06-09 06:11:47 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,7 +41,6 @@ export default function RootLayout({
                 <Sonner />
               </LanguageProvider>
             </TooltipProvider>
-          </ThemeProvider>
         </QueryProvider>
       </body>
     </html>
